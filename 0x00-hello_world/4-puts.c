55077fe510dd9cb5d5219c86ddc6ef87e9c6aadf
ogygold <ogechigrace103@gmail.com>
2022-04-07 13:56:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/**
+ * main - a program that prints a statement
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
