f706be9474d9932bb3a11335b7904ba42d82e785
ogygold <ogechigrace103@gmail.com>
2022-04-12 14:03:59 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * printing alphabets in lowercase
+ */
+void print_alphabets(void)
+{
+char d;
+for (d = 'a'; d <= 'z'; d++)
+_putchar(d);
+_putchar('\n');
+}
