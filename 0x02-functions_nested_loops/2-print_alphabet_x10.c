7df9a9b06262903ab6153eb0e1d40a6cc750e325
ogygold <ogechigrace103@gmail.com>
2022-04-13 09:50:32 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,25 @@
 /**
  * print_alphabet_x10 - prints the alphabet in lowercase 10 times.
  *
- * Return: void
+ * Return: 0 when successful
  */
 void print_alphabet_x10(void)
 {
-	char c;
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 1; i <= 10; i++)
+
 	{
+
+		char c;
+
+
 		for (c = 'a'; c <= 'z'; c++)
+		{
+
 			_putchar(c);
+		}
 		_putchar('\n');
+
 	}
 }
