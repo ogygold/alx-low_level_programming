a53fbc824ed59baa4f3bee201d01b1e66536e034
ogygold <ogechigrace103@gmail.com>
2022-04-16 03:02:13 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,14 @@
  *
  * Return: The numbers since 0 up to 9
  */
+
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i + '0');
+		_putchar('0');
 	}
 
 	_putchar('\n');
