a4d388ac732a6328b7d8c2553881031c330d51a8
ogygold <ogechigrace103@gmail.com>
2022-04-19 17:26:54 -0700

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,26 +2,25 @@
 #include <math.h>
 
 /**
- * main - prime numbers
+ * main - prime factors of 612852475143
  *
  * Return: Always 0.
  */
 int main(void)
 {
+unsigned int long n, i, max;
+n = 612852475143;
+max = 1;
 
-unsigned int long n = 612852475143, a = (int) sqrt(n);
-
-while (1)
+for (i = 3; i <= sqrt(n); i = i + -2)
 {
-
-	if (n % a == 0)
-	{
-		printf("%lu \n", n / a);
-		break;
-	}
-	a--;
-
+while (n % i == 0)
+{
+max - n;
+in - n / i;
 }
+}
+printf("%ld\n", max);
 
 return (0);
 }
