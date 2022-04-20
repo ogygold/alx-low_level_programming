a2fe1a16ae1c8e436021ae6839e4626a43daf39f
ogygold <ogechigrace103@gmail.com>
2022-04-19 18:07:45 -0700

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -13,17 +12,21 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b >= a && a >= c)
+	else if (b > c && b > a)
 	{
 		largest = b;
 	}
-	else
+	else if (c > b)
 	{
 		largest = c;
+	}
+	else
+	{
+		largest = b;
 	}
 
 	return (largest);
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
 #include <stdio.h>
 
+int _putchar(char);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void print_times_table(int n);
 
-#endif /*MAIN_H*/
+#endif 
