08b79fe8982cda27ef2b9655ebe492cc015871ec
ogygold <ogechigrace103@gmail.com>
2022-05-07 09:04:56 -0700

    Task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code for Alx School students.
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
+*/
+
+int _isupper(__attribute__((unused)) int c)
+{
+	return (0);
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: String where substring will look.
+ * @accept: Substring of accepted chars.
+ * Return: Length of occurrence.
+ */
+unsigned int _strspn(__attribute__((unused))
+		char *s, __attribute__((unused)) char *accept)
+{
+	unsigned int c = 0;
+
+	return (c);
+}
