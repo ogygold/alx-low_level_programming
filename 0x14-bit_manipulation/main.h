543b2d2edc59af3e788055d62f64251e7ac72f14
ogygold <ogechigrace103@gmail.com>
2022-05-30 20:14:46 -0700

    Task 6

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,14 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef BIT_MANIPULATION_H_
+#define BIT_MANIPULATION_H_
 
-#define BYTE_LENGHT 8
+#include <unistd.h>
+#include <stdio.h>
 
-int _putchar(char c);
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
-#endif
+#endif /* #ifndef BIT_MANIPULATION_H_ */
