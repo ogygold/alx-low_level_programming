ad63a335927628e43c8bee11921e17eff1a1239a
ogygold <ogechigrace103@gmail.com>
2022-06-14 14:24:51 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,15 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nbr_nodes = 0;
-	const list_t *current = h;
+size_t nbr_nodes = 0;
+const list_t *current = h;
 
-	while (current != NULL)
-	{
-		printf("[%d] %s\n", current->len,
-		current->str != NULL ? current->str : "(nil)");
-		current = current->next;
-		nbr_nodes++;
-	}
-	return (nbr_nodes);
+while (current != NULL)
+{
+	printf("[%d] %s\n", current->len,
+	current->str != NULL ? current->str : "(nil)");
+	current = current->next;
+	nbr_nodes++;
+}
+return (nbr_nodes);
 }
