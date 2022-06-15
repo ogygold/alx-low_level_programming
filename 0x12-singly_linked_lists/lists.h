cc5ddc75cdb3c9fa19c11748fdf16ffcbc8a5656
ogygold <ogechigrace103@gmail.com>
2022-06-14 18:25:14 -0700

    Task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,8 +21,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 }list_t;
-
-int _strlen(const char *str);
+int _putchar(char);
+int _string_length(const char *pointer);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
